e9f29379ca3f443972765b2638c0db9ef5ea0385
Dorra-10 <161203207+Dorra-10@users.noreply.github.com>
2024-08-30 18:03:20 +0100

    Add files via upload

---
+++ songList.js
@@ -0,0 +1,71 @@
+let allMusic=[
+
+{
+  name:"Faded",
+  artist:"Alan Walker",
+  img:"img1.jpg",
+  src:"music1"
+},
+{
+  name:"Mockingbird",
+  artist:"Eminem",
+  img:"img2.jpg",
+  src:"music2"
+},
+{
+  name:"Believer",
+  artist:"Imagine Dragons",
+  img:"img3.jpg",
+  src:"music3"
+},
+{
+  name:"Save Your Tears",
+  artist:"The Weeknd",
+  img:"img4.jpg",
+  src:"music4"
+},
+{
+  name:"Snowman",
+  artist:"Sia",
+  img:"img5.jpg",
+  src:"music5"
+},
+{
+  name:"Blank Space",
+  artist:"Taylor Swift",
+  img:"img6.jpg",
+  src:"music6"
+},
+{
+  name:" Diamonds",
+  artist:"Rihanna",
+  img:"img7.jpg",
+  src:"music7"
+},
+{
+  name:"7 Rings",
+  artist:"Ariana Grande",
+  img:"img8.jpg",
+  src:"music8"
+},
+{
+  name:"Easy On Me",
+  artist:"Adele",
+  img:"img9.jpg",
+  src:"music9"
+},
+{
+  name:"Don't Blame Me",
+  artist:"Taylor Swift",
+  img:"img10.jpg",
+  src:"music10"
+},
+{
+  name:"Flowers",
+  artist:"Miley Cyrus",
+  img:"img11.jpg",
+  src:"music11"
+},
+
+
+];
